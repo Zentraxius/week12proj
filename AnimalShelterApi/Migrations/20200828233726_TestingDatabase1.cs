71657c31598a8070aa81fdba6591049baadb885e
Cody Fritz <clanalia55@gmail.com>
2020-08-28 16:45:29 -0700

    update readme with more information regarding API

---
+++ AnimalShelterApi/Migrations/20200828233726_TestingDatabase1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class TestingDatabase1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
